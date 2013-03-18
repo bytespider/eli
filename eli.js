d3c58dec4f46365c4840334f134fd4bc4222b27c
Rob Griffiths <rob@bytespider.eu>
2013-03-18 15:19:22 +0000

    removed demo code and wrote simple watcher implimentation

--- eli.js
+++ eli.js
@@ -1,15 +1,43 @@
-var pubsub = require('redis-pubsub');
-var config = require('config');
+var client = createDBConnection();
+var pubsub = createDBConnection();
 
-var host = config.server.host || 'localhost';
-var port = config.server.port || 6379;
+client.smembers('posts.publish', function (err, obj) {
+    if (err) {
+        throw new Error(err);
+    }
 
-// Subscribe to channel 'foobar' on a local server.
-var channel = pubsub.createChannel(port, host, 'posts.publish.*');
-channel.on('ready', function() {
-    channel.on('message', function(msg) {
-        console.log(msg.greeting);
-        channel.end();
+    obj.forEach(function (post) {
+        createStatic(post);
     });
-    channel.send({ greeting: 'Hello world!' });
 });
+client.smembers('posts.draft', function (err, obj) { /* do nothing with drafts for now */ });
+
+pubsub.psubscribe('posts.publish.*');
+pubsub.psubscribe('posts.draft.*');
+
+pubsub.on('pmessage', function (pattern, channel, message) {
+    var matches = channel.match(/posts.(publish|draft).(\d{4}\d{2}\d{2})/);
+    client.sadd('posts.' + matches[1], matches[2]);
+    channel_handlers[pattern](message);
+});
+
+function createDBConnection() {
+    var redis   = require('redis');
+    var config  = require('./config');
+
+    var host    = config.db.host    || 'localhost';
+    var port    = config.db.port    || 6379;
+    var options = config.db.options || null;
+
+    return redis.createClient(port, host, options);
+}
+
+function createStatic(post) {
+    console.log(post);
+}
+
+var channel_handlers = {
+    'posts.publish.*': function (message) {
+        console.log(message);
+    }
+};
